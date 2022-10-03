cae335788b60037bfbcda099ecbe09ccfa84f9ea
justnicolas <78233186+justniicolas@users.noreply.github.com>
2022-10-03 20:34:37 +0200

    Update Bibliotheque projet.py

--- Bibliotheque projet.py
+++ Bibliotheque projet.py
@@ -3,6 +3,9 @@ import mysql.connector as MC
 conn = MC.connect(host="localhost",user="root",password="1234", database="biblio")
 cursor = conn.cursor()
 
+
+requete = input("\nQue voulez-vous faire ?\n [1] Recherche d'une oeuvre en fonction de son titre\n [2]Réservation d'une oeuvre\n")
+
 try:
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS bibliotheque (
@@ -34,8 +37,6 @@ try:
         ("Puits sans fond", "2", "Nathan Barber", "Eyrolles", "2019-04-23 12:35:58"),
         ("Trois dans un appartement", "4", "Alfonso Fuentes", "Bayard", "2019-06-16 12:35:58"),
         ("Vol de nuit", "2", "Marshall Mccoy", "Editions Atlas", "2019-06-16 12:35:58"),
-
-
     ]
     
     for i in livres:
@@ -47,16 +48,31 @@ try:
     for i in resultat:
         print(i)
 
-    '''requete = input("\nQue voulez-vous faire ?\n [1] Recherche d'une oeuvre en fonction de son titre\n [2]Réservation d'une oeuvre\n")'''
+    if requete == '1' : 
+        titre_livre_recherche = input('Quelle est le titre de votre livre ? ')
+        cursor.execute("""SELECT id, titre_livre, code_rayon, auteurs, editeurs, date_acquisition FROM bibliotheque WHERE titre_livre = LIKE  '%' + titre_livre_recherche + '% """)
+        resultat = cursor.fetchall()
+        for row in resultat:
+            print('{0}, {1}, {2}, {3}, {4}, {5},'.format(row[0], row[1], row[2], row[3], row[4], row[5]))
+        
 
-
+    if requete == '2' :
+        print("Pas disponible")
+        
 
 except Exception as e:
     print(e)
 
+ 
+
 finally:
     conn.commit()
     conn.close()
+
+
+
+
+
 
 
 
