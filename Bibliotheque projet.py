d8b8fdf07de89cdc53543420068a5201f0edf5ab
justnicolas <78233186+justniicolas@users.noreply.github.com>
2022-10-11 15:16:51 +0200

    Update Bibliotheque projet.py

--- Bibliotheque projet.py
+++ Bibliotheque projet.py
@@ -1,35 +1,51 @@
-import mysql.connector as MC 
+import mysql.connector as MC
 
-conn = MC.connect(host="localhost",user="root",password="1234", database="biblio")
+############################################
+######## CONNEXION AU SERVEUR MYSQL ########
+############################################
+
+conn = MC.connect(host="localhost",user="root",password="78110Bs78")
 cursor = conn.cursor()
 
-
+try:
+    cursor.execute("USE biblio_eh_ns;")
+except:
+    cursor.execute("CREATE DATABASE IF NOT EXISTS biblio_eh_ns;")
+    conn.commit()
+    cursor.execute("USE biblio_eh_ns;")
 
 try:
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS bibliotheque (
-        id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
-        titre_livre VARCHAR(50) NOT NULL,
-        code_rayon INT NOT NULL,
-        auteurs VARCHAR(50) NOT NULL, 
-        editeurs VARCHAR(50) NOT NULL,
-        date_acquisition DATETIME NOT NULL,
-        etat VARCHAR(10), 
-        reservation INT, 
-        PRIMARY KEY(id)
-    );
-    """)
-    
+    #####################################
+    ######## CREATION DES TABLES ########
+    #####################################
+
+    ### Table contenant les abonnés
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS abonnes (
-        id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+        id_abonne SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
         nom VARCHAR(50) NOT NULL,
         prenom VARCHAR(50) NOT NULL,
         age INT NOT NULL, 
-        PRIMARY KEY(id)
+        PRIMARY KEY(id_abonne)
     );
     """)
-  
+
+    ### Table contenant les livres
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS bibliotheque (
+        id_livre SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+        titre_livre VARCHAR(50) NOT NULL,
+        code_rayon INT NOT NULL,
+        auteur VARCHAR(50) NOT NULL, 
+        editeur VARCHAR(50) NOT NULL,
+        date_acquisition DATETIME NOT NULL,
+        etat VARCHAR(10), 
+        reservation INT,
+        id_abonne SMALLINT REFERENCES abonnes(id_abonne),
+        PRIMARY KEY(id_livre)
+    );
+    """)
+
     livres = [
         ("Belle", "10", "Marsden Todd", "Editions Atlas", "2019-06-16 12:35:58"),
         ("Isolation totale", "9", "Marsden Todd", "Editions Atlas", "2019-06-26 12:35:58"),
@@ -50,99 +66,136 @@ try:
         ("Trois dans un appartement", "4", "Alfonso Fuentes", "Bayard", "2019-06-16 12:35:58"),
         ("Vol de nuit", "2", "Marshall Mccoy", "Editions Atlas", "2019-06-16 12:35:58"),
     ]
-    
+
+    #######################################################
+    ######## INSERTION DES VALEURS DANS LES TABLES ########
+    #######################################################
     cursor.execute("SELECT * FROM bibliotheque")
     test_livres = cursor.fetchall()
     if len(test_livres) == 0:
         for i in livres:
-            cursor.execute("""INSERT INTO bibliotheque (id, titre_livre, code_rayon, auteurs, editeurs, date_acquisition) VALUES(NULL, %s, %s, %s, %s, %s)""", i)
+            cursor.execute("""INSERT INTO bibliotheque (id_livre, titre_livre, code_rayon, auteur, editeur, date_acquisition) VALUES(NULL, %s, %s, %s, %s, %s)""", i)
+        conn.commit()
 
-
-    cursor.execute("SELECT * FROM bibliotheque")
-    resultat = cursor.fetchall()
-    # for i in resultat:
-    #     print(i)
-    while True:
-        requete = int(input("\nOptions disponibles :\n - [1] Recherche par titre\n - [2] Recherche par auteur\n - [3] Réservation d'une oeuvre\n - [4] Retour d'une oeuvre\n - [5] Nouveau abonné\nQue voulez-vous faire ? "))
-
-        if requete == 1: 
-            titre_livre_recherche = str(input('Quelle est le titre de votre livre ? '))
-            liste_mots = titre_livre_recherche.split()
-            liste_resultats = []
-            for mot in liste_mots:
-                if len(mot) >= 3 and mot != 'les':
-                    cursor.execute(f"SELECT titre_livre, auteurs, editeurs, code_rayon, id FROM bibliotheque WHERE titre_livre LIKE '%{mot}%'")
-                    resultat = cursor.fetchall()
-            if len(resultat) > 0:
-                for row in resultat:
-                    livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}\n"
-                    if livre_data not in liste_resultats:
-                        liste_resultats.append(livre_data)
-                print('Votre recherche a donné les résultats suivants : \n')
-                for res in liste_resultats:
-                    print(res)
-            else:
-                print("Votre recherche n'a donné aucun résultat.")
-        
-        elif requete == 2:
-            auteur_recherche = str(input("Quelle est le nom de l'auteur ? "))
-            liste_mots = auteur_recherche.split()
-            liste_resultats = []
-            for mot in liste_mots:
-                if len(mot) >= 3 and mot != 'les':
-                    cursor.execute(f"SELECT titre_livre, auteurs, editeurs, code_rayon, id FROM bibliotheque WHERE auteurs LIKE '%{mot}%'")
-                    resultat = cursor.fetchall()
-            if len(resultat) > 0:
-                for row in resultat:
-                    livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}\n"
-                    if livre_data not in liste_resultats:
-                        liste_resultats.append(livre_data)
-                print('Votre recherche a donné les résultats suivants : \n')
-                for res in liste_resultats:
-                    print(res)
-            else:
-                print("Votre recherche n'a donné aucun résultat.")
-
-        elif requete == 3:
-            print('')
-            identifiant = int(input("Quel est l'identifiant de votre livre ? "))
-            cursor.execute(f"SELECT * FROM bibliotheque WHERE id = '{identifiant}'")
-            etat = cursor.fetchall()
-            print('')
-            if etat[0][6] == None:
-                cursor.execute(f"UPDATE bibliotheque SET etat = 'reserve' WHERE id = {identifiant}")
-                conn.commit()
-                print('Réservation effectuée avec succès')
-            else:
-                print("Le livre est déjà réservé")
-
-        elif requete == 4:
-            print('')
-            identifiant = int(input("Quel est l'identifiant de votre livre ? "))
-            cursor.execute(f"SELECT * FROM bibliotheque WHERE id = '{identifiant}'")
-            etat = cursor.fetchall()
-            print('')
-            if etat[0][6] == 'reserve':
-                cursor.execute(f"UPDATE bibliotheque SET etat = NULL WHERE id = {identifiant}")
-                conn.commit()
-                print('Retour effectué avec succès')
-            else:
-                print("Le livre n'est pas réservé")
-        
-        elif requete == 5 : 
-            print ('')
-            inscription_abonné = str(input('Vous êtes nouveau ? '))
-            if inscription_abonné == 'oui' or 'Oui' : 
-                nom = str(input('Quel est votre nom ? '))
-                prenom = str(input('Quel est votre prénom ? '))
-                age = int(input('Quel est votre âge ? '))
-                cursor.execute(f"INSERT INTO abonnes (nom, prenom, age) VALUES ('{nom}','{prenom}','{age}')")
-                identifiant_abo = cursor.execute(f"SELECT id FROM abonnes WHERE nom and prenom and age = '{nom}' and '{prenom}' and '{age}'")
-                print("Vous êtes inscrit ! Voici votre identifiant", identifiant_abo)
+    ############################################
+    ######## CONNEXION DE L'UTILISATEUR ########
+    ############################################
+    print('Bienvenue !')
+    hasAccount = int(input("Options disponibles :\n - [1] Oui\n - [2] Non\nAvez-vous déjà un compte ? "))
+    if hasAccount == 1:
+        id_account = int(input("Quel est votre identifiant ? "))
+        prenom_account = str(input("Quel est votre prénom ? ")).lower()
+        cursor.execute(f"SELECT * FROM abonnes WHERE id_abonne = '{id_account}' AND prenom = '{prenom_account}'")
+        account_details = cursor.fetchone()
+    else:
+        prenom_account = str(input("Quel est votre prénom ? ")).lower()
+        nom_account = str(input("Quel est votre nom ? ")).lower()
+        age_account = int(input("Quel âge avez-vous ? "))
+        cursor.execute(f"SELECT * FROM abonnes WHERE nom = '{nom_account}' AND prenom = '{prenom_account}' AND age = '{age_account}'")
+        accountExists = cursor.fetchone()
+        if accountExists:
+            print("Cette personne possède déjà un compte !")
+            account_details = None
+        else:
+            # ERREURS ICI
+            print(nom_account, prenom_account, age_account)
+            cursor.execute(f"INSERT INTO abonnes VALUES (NULL, '{nom_account}', '{prenom_account}', '{age_account}')")
             
-            else : 
-                print("Vous n'êtes pas inscrit")
-           
+            cursor.execute(f"SELECT * FROM abonnes WHERE nom = '{nom_account}' AND prenom = '{prenom_account}' AND age = '{age_account}'")
+            
+            account_details = cursor.fetchone()
+
+    if account_details:
+        ### Si l'utilisateur est connecté
+        print('\nBienvenue sur le portail de votre bibliothèque !\nMenu principal :')
+        while True:
+            requete = int(input("\nOptions disponibles :\n - [1] Recherche par titre\n - [2] Recherche par auteur\n - [3] Réservation d'une oeuvre\n - [4] Retour d'une oeuvre\nQue voulez-vous faire ? "))
+
+            if requete == 1: 
+                print('')
+                titre_livre_recherche = str(input('Quelle est le titre de votre livre ? '))
+                liste_mots = titre_livre_recherche.split()
+                liste_resultats = []
+                for mot in liste_mots:
+                    if len(mot) >= 3 and mot != 'les':
+                        cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE titre_livre LIKE '%{mot}%'")
+                        resultat = cursor.fetchall()
+                try:
+                    if len(resultat) > 0:
+                        for row in resultat:
+                            livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
+                            if livre_data not in liste_resultats:
+                                liste_resultats.append(livre_data)
+                        print('')
+                        print('Votre recherche a donné les résultats suivants :')
+                        for res in liste_resultats:
+                            print('')
+                            print(f" - {res}")
+                        del resultat
+                    else:
+                        print('')
+                        print("Votre recherche n'a donné aucun résultat.")
+                        del resultat
+                except:
+                    print('')
+                    print("Votre recherche n'a donné aucun résultat.")
+                    pass
+            
+            elif requete == 2:
+                print('')
+                auteur_recherche = str(input("Quelle est le nom de l'auteur ? "))
+                liste_mots = auteur_recherche.split()
+                liste_resultats = []
+                for mot in liste_mots:
+                    if len(mot) >= 3 and mot != 'les':
+                        cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE auteur LIKE '%{mot}%'")
+                        resultat = cursor.fetchall()
+                try:
+                    if len(resultat) > 0:
+                        for row in resultat:
+                            livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
+                            if livre_data not in liste_resultats:
+                                liste_resultats.append(livre_data)
+                        print('')
+                        print('Votre recherche a donné les résultats suivants :')
+                        for res in liste_resultats:
+                            print('')
+                            print(f" - {res}")
+                        del resultat
+                    else:
+                        print('')
+                        print("Votre recherche n'a donné aucun résultat.")
+                        del resultat
+                except:
+                    print('')
+                    print("Votre recherche n'a donné aucun résultat.")
+                    pass
+
+            elif requete == 3:
+                print('')
+                identifiant = int(input("Quel est l'identifiant de votre livre ? "))
+                cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
+                etat = cursor.fetchall()
+                print('')
+                if etat[0][6] == None:
+                    cursor.execute(f"UPDATE bibliotheque SET etat = 'reserve' WHERE id_livre = {identifiant}")
+                    conn.commit()
+                    print('Réservation effectuée avec succès')
+                else:
+                    print("Le livre est déjà réservé")
+
+            elif requete == 4:
+                print('')
+                identifiant = int(input("Quel est l'identifiant de votre livre ? "))
+                cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
+                etat = cursor.fetchall()
+                print('')
+                if etat[0][6] == 'reserve':
+                    cursor.execute(f"UPDATE bibliotheque SET etat = NULL WHERE id_livre = {identifiant}")
+                    conn.commit()
+                    print('Retour effectué avec succès')
+                else:
+                    print("Le livre n'est pas réservé")
 
 
 except Exception as e:
