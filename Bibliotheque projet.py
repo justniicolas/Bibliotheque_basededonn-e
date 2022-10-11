c9fe353d87dc83048b9fa5ceb31c890e8f42c7da
justnicolas <78233186+justniicolas@users.noreply.github.com>
2022-10-11 16:37:04 +0200

    Update Bibliotheque projet.py

--- Bibliotheque projet.py
+++ Bibliotheque projet.py
@@ -106,103 +106,140 @@ try:
             account_details = cursor.fetchone()
 
     if account_details:
-        ### Si l'utilisateur est connecté
-        print('\nBienvenue sur le portail de votre bibliothèque !\nMenu principal :')
-        while True:
-            requete = int(input("\nOptions disponibles :\n - [1] Recherche par titre\n - [2] Recherche par auteur\n - [3] Réservation d'une oeuvre\n - [4] Retour d'une oeuvre\nQue voulez-vous faire ? "))
+    ### Si l'utilisateur est connecté
+    print('\nBienvenue sur le portail de votre bibliothèque !\nMenu principal :')
+    while True:
+        requete = int(input("\nOptions disponibles :\n - [1] Recherche par titre\n - [2] Recherche par auteur\n - [3] Réservation d'une oeuvre\n - [4] Retour d'une oeuvre\n - [5] Recherche par éditeur\nQue voulez-vous faire ? "))
 
-            if requete == 1: 
-                print('')
-                titre_livre_recherche = str(input('Quelle est le titre de votre livre ? '))
-                liste_mots = titre_livre_recherche.split()
-                liste_resultats = []
-                for mot in liste_mots:
-                    if len(mot) >= 3 and mot != 'les':
-                        cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE titre_livre LIKE '%{mot}%'")
-                        resultat = cursor.fetchall()
-                try:
-                    if len(resultat) > 0:
-                        for row in resultat:
-                            livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
-                            if livre_data not in liste_resultats:
-                                liste_resultats.append(livre_data)
-                        print('')
-                        print('Votre recherche a donné les résultats suivants :')
-                        for res in liste_resultats:
-                            print('')
-                            print(f" - {res}")
-                        del resultat
-                    else:
-                        print('')
-                        print("Votre recherche n'a donné aucun résultat.")
-                        del resultat
-                except:
+        if requete == 1: 
+            print('')
+            titre_livre_recherche = str(input('Quelle est le titre de votre livre ? '))
+            liste_mots = titre_livre_recherche.split()
+            liste_resultats = []
+            for mot in liste_mots:
+                if len(mot) >= 3 and mot != 'les':
+                    cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE titre_livre LIKE '%{mot}%'")
+                    resultat = cursor.fetchall()
+            try:
+                if len(resultat) > 0:
+                    for row in resultat:
+                        livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
+                        if livre_data not in liste_resultats:
+                            liste_resultats.append(livre_data)
                     print('')
-                    print("Votre recherche n'a donné aucun résultat.")
-                    pass
-            
-            elif requete == 2:
-                print('')
-                auteur_recherche = str(input("Quelle est le nom de l'auteur ? "))
-                liste_mots = auteur_recherche.split()
-                liste_resultats = []
-                for mot in liste_mots:
-                    if len(mot) >= 3 and mot != 'les':
-                        cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE auteur LIKE '%{mot}%'")
-                        resultat = cursor.fetchall()
-                try:
-                    if len(resultat) > 0:
-                        for row in resultat:
-                            livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
-                            if livre_data not in liste_resultats:
-                                liste_resultats.append(livre_data)
+                    print('Votre recherche a donné les résultats suivants :')
+                    for res in liste_resultats:
                         print('')
-                        print('Votre recherche a donné les résultats suivants :')
-                        for res in liste_resultats:
-                            print('')
-                            print(f" - {res}")
-                        del resultat
-                    else:
-                        print('')
-                        print("Votre recherche n'a donné aucun résultat.")
-                        del resultat
-                except:
-                    print('')
-                    print("Votre recherche n'a donné aucun résultat.")
-                    pass
-
-            elif requete == 3:
-                print('')
-                identifiant = int(input("Quel est l'identifiant de votre livre ? "))
-                cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
-                etat = cursor.fetchall()
-                print('')
-                if etat[0][6] == None:
-                    cursor.execute(f"UPDATE bibliotheque SET etat = 'reserve' WHERE id_livre = {identifiant}")
-                    conn.commit()
-                    print('Réservation effectuée avec succès')
+                        print(f" - {res}")
+                    del resultat
                 else:
-                    print("Le livre est déjà réservé")
+                    print('')
+                    print("Votre recherche n'a donné aucun résultat.")
+                    del resultat
+            except:
+                print('')
+                print("Votre recherche n'a donné aucun résultat.")
+                pass
+        
+        elif requete == 2:
+            print('')
+            auteur_recherche = str(input("Quel est le nom de l'auteur ? "))
+            liste_mots = auteur_recherche.split()
+            liste_resultats = []
+            for mot in liste_mots:
+                if len(mot) >= 3 and mot != 'les':
+                    cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE auteur LIKE '%{mot}%'")
+                    resultat = cursor.fetchall()
+            try:
+                if len(resultat) > 0:
+                    for row in resultat:
+                        livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
+                        if livre_data not in liste_resultats:
+                            liste_resultats.append(livre_data)
+                    print('')
+                    print('Votre recherche a donné les résultats suivants :')
+                    for res in liste_resultats:
+                        print('')
+                        print(f" - {res}")
+                    del resultat
+                else:
+                    print('')
+                    print("Votre recherche n'a donné aucun résultat.")
+                    del resultat
+            except:
+                print('')
+                print("Votre recherche n'a donné aucun résultat.")
+                pass
 
-            elif requete == 4:
-                print('')
-                identifiant = int(input("Quel est l'identifiant de votre livre ? "))
-                cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
-                etat = cursor.fetchall()
-                print('')
-                if etat[0][6] == 'reserve':
+        elif requete == 3:
+            print('')
+            identifiant = int(input("Quel est l'identifiant de votre livre ? "))
+            cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
+            etat = cursor.fetchall()
+            print('')
+            if etat[0][6] == None:
+                cursor.execute(f"UPDATE bibliotheque SET etat = 'reserve' WHERE id_livre = {identifiant}")
+                conn.commit()
+                id_abonne = str(input('Quel est votre identifiant de compte ?'))
+                cursor.execute(f"SELECT * FROM abonnes WHERE id_abonne = '{id_abonne}'")
+                if cursor.fetchone():
+                    print('Réservation effectuée avec succès')
+                else : 
+                    print('Réservation non effectuée')
+            else:
+                print("Le livre est déjà réservé")
+
+        elif requete == 4:
+            print('')
+            identifiant = int(input("Quel est l'identifiant de votre livre ? "))
+            cursor.execute(f"SELECT * FROM bibliotheque WHERE id_livre = '{identifiant}'")
+            etat = cursor.fetchall()
+            print('')
+            if etat[0][6] == 'reserve':
+                id_abonne = str(input('Quel est votre identifiant de compte ?'))
+                cursor.execute(f"SELECT * FROM bibliotheque WHERE id_abonne = '{id_abonne} and id_livre = '{identifiant}'")
+                if cursor.fetchone(): 
+                    cursor.execute(f"UPDATE bibliotheque SET etat = NULL WHERE id_livre = {identifiant}")
                     cursor.execute(f"UPDATE bibliotheque SET etat = NULL WHERE id_livre = {identifiant}")
                     conn.commit()
                     print('Retour effectué avec succès')
+                else : 
+                    print ('Il y a une erreur dans votre demande. Veuillez recommencer.')    
+            else:
+                print("Le livre n'est pas réservé")
+
+        elif requete == 5:
+            print('')
+            editeur_recherche = str(input("Quel est l'éditeur ? "))
+            liste_mots = editeur_recherche.split()
+            liste_resultats = []
+            for mot in liste_mots:
+                if len(mot) >= 3 and mot != 'les':
+                    cursor.execute(f"SELECT titre_livre, auteur, editeur, code_rayon, id_livre FROM bibliotheque WHERE editeur LIKE '%{mot}%'")
+                    resultat = cursor.fetchall()
+            try:
+                if len(resultat) > 0:
+                    for row in resultat:
+                        livre_data = f"{row[0]}, écrit par {row[1]}, aux éditions {row[2]} --- Rayon : {row[3]} --- Identifiant : {row[4]}"
+                        if livre_data not in liste_resultats:
+                            liste_resultats.append(livre_data)
+                    print('')
+                    print('Votre recherche a donné les résultats suivants :')
+                    for res in liste_resultats:
+                        print('')
+                        print(f" - {res}")
+                    del resultat
                 else:
-                    print("Le livre n'est pas réservé")
+                    print('')
+                    print("Votre recherche n'a donné aucun résultat.")
+                    del resultat
+            except:
+                print('')
+                print("Votre recherche n'a donné aucun résultat.")
+                pass
 
 
-except Exception as e:
-    print(e)
 
- 
+conn.commit()
+conn.close()
 
-finally:
-    conn.commit()
-    conn.close()
